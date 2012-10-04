19f740b4bf8de8a2bed6332a45d1834dc1885b4f
Duncan McGreggor <oubiwann@mindpool.io>
2012-10-04 10:34:12 -0700

    Added missing file.

---
+++ chapter03/05-prompt-cd.lisp
@@ -0,0 +1,7 @@
+(defun prompt-for-cd ()
+  (make-cd
+    (prompt-read "Title")
+    (prompt-read "Artist")
+    (or (parse-integer (prompt-read "Rating") :junk-allowed t) 0)
+    (y-or-n-p "Ripped [y/n]: ")))
+
