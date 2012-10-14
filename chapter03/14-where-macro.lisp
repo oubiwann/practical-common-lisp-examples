14fc451b6198641dbb38988889d0e3276d14afda
Duncan McGreggor <oubiwann@mindpool.io>
2012-10-13 17:49:25 -0700

    Added missing functions to macro file.

--- chapter03/14-where-macro.lisp
+++ chapter03/14-where-macro.lisp
@@ -1,2 +1,9 @@
+(defun make-comparison-expr (field value)
+  `(equal (getf cd ,field) ,value))
+
+(defun make-comparisons-list (fields)
+  (loop while fields
+        collecting (make-comparison-expr (pop fields) (pop fields))))
+
 (defmacro where (&rest clauses)
   `#'(lambda (cd) (and ,@(make-comparisons-list clauses))))
